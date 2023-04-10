6c2a37012f7d9338021b49950c3a827bfa239901
Nick Seguin <nseguin@orourkesales.com>
2023-04-10 09:27:38 -0500

    remove unused code

--- BugReproduction/MainPage.xaml.cs
+++ BugReproduction/MainPage.xaml.cs
@@ -2,30 +2,6 @@
 
 public partial class MainPage : ContentPage
 {
-	string _searchBarText;
-	string _entryText;
-
-	public string SearchBarText
-	{
-		get => _searchBarText;
-		set
-		{
-			_searchBarText = value;
-			OnPropertyChanged();
-		}
-	}
-
-	public string EntryText
-	{
-		get => _entryText;
-		set
-		{
-			_entryText = value;
-			OnPropertyChanged();
-		}
-	}
-	
-
 	public MainPage()
 	{
 		InitializeComponent();
